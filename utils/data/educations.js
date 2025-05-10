15cc7a5604f1231dd201dc04716461a3ba220bfc
Abhishek Kumar <42512112+abhishekkumar99@users.noreply.github.com>
2025-05-10 11:46:27 +0530

    Add files via upload

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2017 - 2021",
+    institution: "Asansol Engineering College",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2015 - 2017",
+    institution: "Sri Chaitaniya",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2005 - 2015",
+    institution: "DAV Public School",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Developer',
+    company: "Capgemini India",
+    duration: "(Jan 2022 - Present)"
+  },
+  {
+    id: 2,
+    title: ".Net Developer",
+    company: "Symphony Retail AI (Intership)",
+    duration: "(March 2021 - Oct 2022)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2018 - Present)"
+  }
+]
