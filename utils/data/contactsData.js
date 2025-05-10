957e2397cb3eb2f439493f3ab0a249c58b95e4bc
Abhishek Kumar <42512112+abhishekkumar99@users.noreply.github.com>
2025-05-10 11:55:40 +0530

    $$$
    It is a professional digital space designed to showcase my work, skills, and achievements.

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: 'abhishek.au.kumar03@gmail.com',
+    phone: '+91-8016034179',
+    address: 'Sukanto Pally, Asasnol(WB), India - 713302 ',
+    github: 'https://github.com/abhishekkumar99',
+    facebook: 'https://www.salesforce.com/trailblazer/akumar5183',
+    linkedIn: 'https://www.linkedin.com/in/abhishek-kumar-b79890180/',
+    twitter: "https://www.salesforce.com/trailblazer/akumar5183",
+    stackOverflow: 'https://www.salesforce.com/trailblazer/akumar5183',
+    leetcode: "https://www.salesforce.com/trailblazer/akumar5183",
+    devUsername: "abhishek_kumar_66cbb493de"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Abhishek Kumar",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Abhishek Kumar. I am a professional and enthusiastic programmer in my daily life. Dynamic and results-driven Salesforce Developer with over 3 years of experience designing, implementing, and maintaining Salesforce applications. Expertise in customizing Salesforce platform components, Apex, and Lightning Web Components (LWC). Strong problem-solving skills and a keen ability to collaborate with business stakeholders to deliver seamless CRM solutions. Seeking to leverage my Salesforce expertise to contribute to the success of a growth-oriented organization.",
+  email: 'abhishek.au.kumar03@gmail.com', 
+  phone: '+91-8016034179',
+  address: 'Sukanto Pally, Asasnol(WB), India - 713302',
+  github: 'https://github.com/abhishekkumar99',
+  facebook: 'https://www.salesforce.com/trailblazer/akumar5183',
+  linkedIn: 'https://www.linkedin.com/in/abhishek-kumar-b79890180/',
+  twitter: "https://www.salesforce.com/trailblazer/akumar5183",
+  stackOverflow: 'https://www.salesforce.com/trailblazer/akumar5183',
+  leetcode: "https://www.salesforce.com/trailblazer/akumar5183",
+  devUsername: "abhishek_kumar_66cbb493de",
+  resume: "https://drive.google.com/file/d/1XjdE7cvi5lrW2xAbcf1MzFIv_T6_V_-Y/view?usp=sharing"
+}
